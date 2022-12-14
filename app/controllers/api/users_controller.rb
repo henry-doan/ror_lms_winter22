c3c77310b1d2be3ce97fcc2b88a25dd921576fa2
Henry Doan <henrydoan96@gmail.com>
2022-12-13 18:11:48 -0700

    user backend done

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,16 +1,43 @@
 class Api::UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy]
+  
   def index
+    render json: User.all
   end
 
   def show
+    render json: @user
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save 
+      render json: @user 
+    else
+      render json: { errors: @user.error }, status: :unprocessable_entity
+    end
   end
 
   def update
+    if @user.update(user_params)
+      render json: @user 
+    else
+      render json: { errors: @user.error }, status: :unprocessable_entity
+    end
   end
 
   def destroy
+    @user.destroy
+    render json: { message: 'User Deleted' }
   end
+
+  private 
+    def set_user
+      @user = User.find(params[:id])
+    end
+
+    def user_params
+      params.require(:user).permit(:first_name, :last_name, :email, :password)
+    end
 end
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,5 @@
 Course.delete_all
+User.delete_all
 
 10.times do
   Course.create(
@@ -7,7 +8,17 @@ Course.delete_all
     ctype: Faker::Educator.subject,
     course_number: Faker::Number.number(digits: 4)
   )
+
+  User.create(
+    first_name: Faker::FunnyName.name,
+    last_name: Faker::FunnyName.name,
+    email: "#{Faker::Tea.variety}@email.com",
+    password: 'password'
+  )
 end
 
 puts "# Courses:"
 puts Course.all.count
+
+puts "# Users:"
+puts User.all.count
